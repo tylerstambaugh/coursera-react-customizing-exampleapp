339c524a9a461efbf25d230d57dd32e5fe5581d0
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-03-12 18:06:10 -0400

    changing bool value with onClick

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import Btn from './components/examples/btn';
 import './App.css';
 import Nav from './components/Nav.js';
 import Example from './components/examples/Example.js';
 import Card from './components/examples/Card.js';
+import Btn from './components/examples/btn';
+import ModeToggler from './components/examples/ModeToggler';
 
 
 function App() {
   return (
-    <Btn />
+    <ModeToggler />
   );
 }
 
