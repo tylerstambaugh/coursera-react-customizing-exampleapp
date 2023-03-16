fdf5c98cb4319705db972fc6895149adf1ed3381
Tyler stambaugh <11870675+tylerstambaugh@users.noreply.github.co>
2023-03-16 14:33:26 -0400

    first use of reducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import './App.css';
 import React, { useState} from 'react';
-import MealsProvider from './providers/MealsProvider';
-import MealsList from './components/examples/MealsList';
-import Counter from './components/examples/Counter';
+import { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  if (action.type === 'ride') return {money: state.money + 10};
+  if (action.type === 'fuel') return {money: state.money - 50};
+  return new Error();
+}
+
+function App() {
+
+  const initialState = {money: 100};
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div className="App">
+      <h1>Wallet: {state.money}</h1>
+      <div>
+        <button onClick={() => dispatch({type: 'ride'})}>
+          A new customer!
+        </button>
+        <button onClick={() => dispatch({type: 'fuel'})}>
+          Refill the tank!
+        </button>
+      </div>
+
+
+    </div>
+
+  );
+}
+
+
+
+export default App;
+
+// import MealsProvider from './providers/MealsProvider';
+// import MealsList from './components/examples/MealsList';
+// import Counter from './components/examples/Counter';
 
 // import LittleLemons from './components/examples/LittleLemons';
 // import InputComponent from './components/examples/InputComponent';
@@ -13,21 +48,17 @@ import Counter from './components/examples/Counter';
 // import Btn from './components/examples/btn';
 // import ModeToggler from './components/examples/ModeToggler';
 
-function App() {
-  return (
-    <div>
-      <MealsProvider>
-        <MealsList />
-        <Counter />
+// function App() {
+//   return (
+//     <div>
+//       <MealsProvider>
+//         <MealsList />
+//         <Counter />
 
-      </MealsProvider>
-    </div>
-  );
-}
-
-
-export default App;
-
+//       </MealsProvider>
+//     </div>
+//   );
+// }
 
 
 // function App() {
